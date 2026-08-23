f0a00755bf3ae4a6df910e52d8265cce2a147131
Olivier Belhomme <olivier@belhomme.org>
2026-08-23 12:00:02 +0200

    build: use remote Nerves system by default

--- alfredpi/mix.exs
+++ alfredpi/mix.exs
@@ -59,12 +59,8 @@ defmodule Alfredpi.MixProject do
       # {:nerves_system_rpi3a, "~> 1.24", runtime: false, targets: :rpi3a}
       {:rabbit_manager, path: "../rabbit_manager"},
       {:alfredpi_ui, path: "../alfredpi_ui"},
-      # {:alfredpi_rpi3a,
-      #  path: "../../nerves_systems/src/alfredpi_rpi3a",
-      #  runtime: false,
-      #  targets: :alfredpi_rpi3a,
-      #  nerves: [compile: true]}
       {:alfredpi_rpi3a, github: "olb17/alfredpi_rpi3a", runtime: false, targets: :alfredpi_rpi3a}
+
     ]
   end
 
